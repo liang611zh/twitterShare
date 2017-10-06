025b730650cef49e648313d8ada1c786843f0bda
LIANG ZHAO <liangzhao@node-1w7jr9qqdi59sngj3yjwxefdu.ipv6.telus.net>
2017-10-05 18:27:16 -0700

    Initial Commit

---
+++ twitterDemo1/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  twitterDemo1
+//
+//  Created by LIANG ZHAO on 2017-10-05.
+//  Copyright © 2017 bcit. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
